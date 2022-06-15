1fac961c9dd27c827fcfbdbf866133493b945dfe
AasinR <bazsi2001018@gmail.com>
2022-06-15 20:19:05 +0200

    found github api

--- app/src/components/data/DataPage.tsx
+++ app/src/components/data/DataPage.tsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from 'axios';
 
 function DataPage() {
+    const [dataFiles, setDataFiles] = useState<any[]>([]);
+    
+    useEffect(() => {
+        axios.get("https://api.github.com/repos/AasinR/random-data/contents/data")
+        .then(res => {
+            setDataFiles(res.data);
+            console.log(res.data);
+        });
+    }, []);
+
+    const items: any[] = [];
+    dataFiles.forEach(data => {
+        items.push(
+            <p className="test"><a href={data.download_url}>{data.name}</a></p>
+        )
+    });
+
     return (
         <div>
-            data page
+            {items}
         </div>
     );
 }
