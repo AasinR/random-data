d905ff2ee658605d85201299f229f4d598b570da
AasinR <bazsi2001018@gmail.com>
2022-06-16 01:09:32 +0200

    updated navbar

--- app/src/components/navbar/NavBar.tsx
+++ app/src/components/navbar/NavBar.tsx
@@ -1,13 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 import "./NavBar.css";
 
+
 function NavBar() {
+    const menuItems = [
+        {
+            title: "Data",
+            url: "/data",
+            cName: "nav-link"
+        },
+        {
+            title: "Home",
+            url: "/",
+            cName: "nav-link"
+        }
+    ]
+
+    let [clicked, setClicked] = useState<boolean>(false);
+
+    const handleClick = () => {
+        setClicked(!clicked);
+    }
+    
     return (
         <nav className="nav">
             <h1 className="nav-logo">Navbar</h1>
-            <ul className="nav-links">
-                <li><a href="/data">Data</a></li>
-                <li><a href="/">Home</a></li>
+            <div className="menu-icon" onClick={handleClick}>
+                <FontAwesomeIcon icon={clicked ? faTimes : faBars}></FontAwesomeIcon>
+            </div>
+            <ul className={clicked ? "nav-menu active" : "nav-menu"}>
+                {menuItems.map((item, index) => {
+                    return (
+                        <li key={index}>
+                            <a className={item.cName} href={item.url}>
+                                {item.title}
+                            </a>
+                        </li>
+                    );
+                })}
             </ul>
         </nav>
     );
