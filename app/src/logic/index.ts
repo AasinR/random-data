f5b0832538032245e0d153236ef8643fe1ad1f41
AasinR <bazsi2001018@gmail.com>
2022-07-13 02:02:39 +0200

    added random functions

--- app/src/logic/index.ts
+++ app/src/logic/index.ts
@@ -1,2 +1,3 @@
 export {default as downloadFile} from "./downloadFile";
 export {default as formatName} from "./formatName";
+export {default as random} from "./random";
---
+++ app/src/logic/random.ts
@@ -0,0 +1,23 @@
+/**
+ * Returns a random number between the given values.
+ */
+function randInt(minValue: number, maxValue: number) {
+    return Math.floor(Math.random() * (maxValue - minValue) + minValue);
+}
+
+/**
+ * Returns one or the given amount of random values from an array.
+ */
+function choice(array: any[], number: number = 1) {
+    const length: number = array.length;
+    if (number === 1) {
+        return array[Math.floor(Math.random() * length)];
+    }
+    let result: any[] = [];
+    for (let i = 0; i < number; i++) {
+        result.push(array[Math.floor(Math.random() * length)]);
+    }
+    return result;
+}
+
+export default { randInt, choice };
