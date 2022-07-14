ef64596b34b3fc1774741dd3f682b7607037b60e
AasinR <bazsi2001018@gmail.com>
2022-07-15 01:39:57 +0200

    added formatDate function

---
+++ app/src/logic/formatDate.ts
@@ -0,0 +1,38 @@
+function formatDate(date: Date, format: string, separator: string) {
+    const array: string[] = format.split("-");
+    let dates: string[] = [];
+
+    function select(_format: string) {
+        let dateString: string = "";
+
+        switch (_format) {
+            case "yyyy":
+                dateString = date.getFullYear().toString();
+                break;
+            case "yy":
+                dateString = date.getFullYear().toString().substring(2, 4);
+                break;
+            case "mm":
+                dateString = (date.getMonth() + 1).toString().padStart(2, "0");
+                break;
+            case "m":
+                dateString = (date.getMonth() + 1).toString();
+                break;
+            case "dd":
+                dateString = date.getDate().toString().padStart(2, "0");
+                break;
+            case "d":
+                date.getDate().toString();
+        }
+
+        return dateString;
+    }
+
+    array.forEach((item: string) => {
+        dates.push(select(item))
+    });
+
+    return dates.join(separator);
+}
+
+export default formatDate;
--- app/src/logic/index.ts
+++ app/src/logic/index.ts
@@ -1,5 +1,6 @@
 export { default as downloadFile } from "./downloadFile";
 export { default as formatName } from "./formatName";
+export { default as formatDate } from "./formatDate";
 export { default as random } from "./random";
 export { default as randomData } from "./randomData";
 export { randPassword } from "./randomData";
